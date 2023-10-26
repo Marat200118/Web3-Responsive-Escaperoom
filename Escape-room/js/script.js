e7db3c139e990652f50056d549afb53b235b50d2
Marat200118 <marat.samigullin1@gmail.com>
2023-10-26 09:20:35 +0200

    third and fourth task added

--- Escape-room/js/script.js
+++ Escape-room/js/script.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputFields = document.querySelectorAll('input[type="text"]');
   const firstRobber = questSteps[0].querySelector(".robber");
   const secondRobber = questSteps[1].querySelector(".robber");
-  const thirdRobber = questSteps[2].querySelector(".robber"); // Fixed the indexing here
+  const thirdRobber = questSteps[2].querySelector(".robber");
+  const fourthRobber = questSteps[3].querySelector(".robber"); // Fixed the indexing here
 
   digit3.addEventListener("click", function () {
     inputFields[0].value = "3";
@@ -24,6 +25,22 @@ document.addEventListener("DOMContentLoaded", function () {
       questSteps[1].classList.add("completed");
       secondRobber.classList.remove("visible");
       thirdRobber.classList.add("visible");
+    }
+  });
+
+  inputFields[2].addEventListener("input", function () {
+    if (inputFields[2].value == "5") {
+      questSteps[2].classList.add("completed");
+      thirdRobber.classList.remove("visible");
+      fourthRobber.classList.add("visible");
+    }
+  });
+
+  inputFields[3].addEventListener("input", function () {
+    if (inputFields[3].value == "2") {
+      questSteps[3].classList.add("completed");
+      thirdRobber.classList.remove("visible");
+      // fourthRobber.classList.add("visible");
     }
   });
 });
